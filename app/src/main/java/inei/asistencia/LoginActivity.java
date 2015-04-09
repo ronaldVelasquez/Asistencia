e3874257ffd40f08ff0830d85484d1268be47b00
ronaldVelasquez <ronaldvg93@gmail.com>
2015-04-09 16:28:05 -0500

    Add login

---
+++ app/src/main/java/inei/asistencia/LoginActivity.java
@@ -0,0 +1,29 @@
+package inei.asistencia;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class LoginActivity extends Activity {
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UbigeoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
